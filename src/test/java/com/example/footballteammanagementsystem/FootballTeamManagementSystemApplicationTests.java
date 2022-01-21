db996efcb1a43142a6b653bb396a0539aa87a623
Zigix <olekwszk7@gmail.com>
2022-01-21 13:28:32 +0100

    Initial commit

---
+++ src/test/java/com/example/footballteammanagementsystem/FootballTeamManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballteammanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
